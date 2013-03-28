0a76cd8efb5792aabbd8e44795bfd82a8585dcee
sezam <sezamo@gmail.com>
2013-03-28 22:57:43 +0600

    настроил hibernate

---
+++ src/main/java/com/youdoc/entity/impl/SimpleUserImpl.java
@@ -0,0 +1,92 @@
+package com.youdoc.entity.impl;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.youdoc.entity.User;
+
+/**
+ * Простой пользователь системы. Имеет логин, пароль и email. 
+ * 
+ * @author sezam
+ * 
+ */
+@Entity
+@Table(name = "T_SIMPLEUSER")
+public class SimpleUserImpl implements User  {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int pid;
+
+	private String login;
+	private String password;
+	private String email;
+
+	/**
+	 * @return the pid
+	 */
+	public int getPid() {
+		return pid;
+	}
+
+	/**
+	 * @param pid
+	 *            the pid to set
+	 */
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.youdoc.entity.impl.User#getLogin()
+	 */
+	@Override
+	public String getLogin() {
+		return login;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.youdoc.entity.impl.User#setLogin(java.lang.String)
+	 */
+	@Override
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.youdoc.entity.impl.User#getPassword()
+	 */
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.youdoc.entity.impl.User#setPassword(java.lang.String)
+	 */
+	@Override
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.youdoc.entity.impl.User#getEmail()
+	 */
+	@Override
+	public String getEmail() {
+		return email;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.youdoc.entity.impl.User#setEmail(java.lang.String)
+	 */
+	@Override
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
---
+++ src/main/java/com/youdoc/service/UserService.java
@@ -0,0 +1,37 @@
+package com.youdoc.service;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.youdoc.entity.User;
+
+/**
+ * Сервис для User.
+ * 
+ * @author sezam
+ * 
+ */
+public class UserService {
+	private SessionFactory sessionFactory;
+
+	/**
+	 * @param sessionFactory
+	 *            the sessionFactory to set
+	 */
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	/**
+	 * Добавиьт и сохранить @User.
+	 * 
+	 * @param user
+	 */
+	@Transactional(readOnly = false)
+	public void addUser(User user) {
+		Session session = sessionFactory.openSession();
+		session.save(user);
+		session.close();
+	}
+}
